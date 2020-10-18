d2c45c2d72c38e43d62e8c117df6d5ccc843ad2f
Shuyuan Dong <sd3416@columbia.edu>
2020-10-18 17:43:49 +0000

    Add an sightings/add Version1

--- project/squirrel/my_form.py
+++ project/squirrel/my_form.py
@@ -2,12 +2,12 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 class EmpForm(forms.Form):
-    Longitude = forms.FloatField(label='longitude', error_messages={"required": "The field cannot be empty!"})
-    Latitude = forms.FloatField(label="latitude", error_messages={"required": "The field cannot be empty!"})
-    Unique_Squirrel_ID = forms.CharField(label="Unique_Squirrel_ID", error_messages={"required": "The field cannot be empty!"})
-    Shift = forms.CharField(label="Shift", error_messages={"required": "The field cannot be empty and can only be 'AM' or 'PM'"})
-    Date = forms.CharField(min_length=8, max_length = 8, label = "Date", error_messages={"min_length":"Date length must be 8", "max_length":"Date length must be 8", "required": "The field cannot be empty."})
-    Age = forms.CharField(label="Age", error_messages={"required": "The field cannot be empty and can only be 'Adult' or 'Juvenile'"})
+    Longitude = forms.FloatField(label='longitude', error_messages={"required": "This field cannot be empty!"})
+    Latitude = forms.FloatField(label="latitude", error_messages={"required": "This field cannot be empty!"})
+    Unique_Squirrel_ID = forms.CharField(label="Unique_Squirrel_ID", error_messages={"required": "This field cannot be empty!"})
+    Shift = forms.CharField(label="Shift", error_messages={"required": "This field cannot be empty and can only be 'AM' or 'PM'"})
+    Date = forms.CharField(min_length=8, max_length = 8, label = "Date", error_messages={"min_length":"Date length must be 8", "max_length":"Date length must be 8", "required": "This field cannot be empty!"})
+    Age = forms.CharField(label="Age", error_messages={"required": "This field cannot be empty and can only be 'Adult' or 'Juvenile'"})
     Primary_Fur_Color = forms.CharField(label="Primary_Fur_Color", required=False)
     Location = forms.CharField(label="Location", required=False)
     Specific_Location = forms.CharField(label="Specific_Location", required=False)
