561e3e2ce5558517a42789222c19595a89b0a3f3
Yunlin Li <yl4547@columbia.edu>
2020-10-19 23:55:25 +0000

    add stats url

--- project/squirrel/urls.py
+++ project/squirrel/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('sightings/detail/<str:sid>', views.detail, name='detail'),
     path('sightings/add/', views.add, name='add'),
     path('map/', views.map, name='map'),
+    path('sightings/stats/', views.stats,name='stats'),
 ]
 
